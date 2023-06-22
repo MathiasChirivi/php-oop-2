a670fa96693074d668b8cb936a4836c68796604b
MathiasChirivi <chirivimathias3@gmail.com>
2023-06-22 15:49:28 +0200

    fix layout

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $PlaysListDog = [
 ];
 
 $DogsBedList = [
-    new Dogsbed('cuccia', 30, 'Cuscino double-face estraibile, particolarmente morbido e avvolgente', $categoriaGatto , 'https://www.aquazoomaniashop.it/34082-large_default/dog-line-cuccia-svalbard-waterproof-per-cane.jpg', '50L x 37l x 17Sp cm', 'tipo', 'Cotone polipropilene'),
+    new Dogsbed('cuccia', 30, 'Cuscino double-face estraibile, particolarmente morbido e avvolgente', $categoriaGatto, 'https://www.aquazoomaniashop.it/34082-large_default/dog-line-cuccia-svalbard-waterproof-per-cane.jpg', '50L x 37l x 17Sp cm', 'tipo', 'Cotone polipropilene'),
 ];
 
 $FoodList = [
@@ -29,6 +29,7 @@ $FoodList = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 
@@ -41,23 +42,28 @@ $FoodList = [
     <main>
         <div>
             <h3 class="pb-2">Scegli tra i tanti giochi disponibili per il tuo Animale</h3>
-            <div class="d-flex">
-                <?php foreach ($PlaysListDog as $play) { ?>
-                    <div class="d-flex">
-                        <div class="card" style="width: 18rem;">
+            <?php foreach ($PlaysListDog as $play) { ?>
+                <div class="card " style="width: 18rem;">
+                    <div class="container">
+                        <div class="d-flex flex-column justify-content-between">
                             <img src="<?= $play->imgProduct ?>" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text m-0">Categoria: <?= $play->category->icon; $play->category->name ?></p>
+                            <div class="card-body text-center">
+                                <p class="card-text m-0 mb-3"><strong>Categoria:</strong> <?= $play->category->icon; $play->category->name ?></p>
                                 <p class="card-text"><strong>Gioco:</strong> <?= $play->name ?></p>
-                                <p class="card-text"><strong>Prezzo Gioco:</strong> <?= $play->getPrice() ?></p>
-                                <p class="card-text"><strong>Descrizione Gioco:</strong> <?= $play->description ?></p>
-                                <p class="card-text"><strong>Dimensione Gioco:</strong> <?= $play->ProductDimensions ?></p>
-                                <a href="#" class="btn btn-primary">Acquista Prodotto</a>
+                                <div class="collapse" id="collapseExample">
+                                    <p class="card-text"><strong>Prezzo Gioco:</strong> <?= $play->getPrice() ?></p>
+                                    <p class="card-text"><strong>Descrizione Gioco:</strong> <?= $play->description ?></p>
+                                    <p class="card-text"><strong>Dimensione Gioco:</strong> <?= $play->ProductDimensions ?></p>
+                                    <a href="#" class="btn btn-primary">Acquista Prodotto</a>
+                                </div>
+                            </div>
+                            <div class="text-center mb-3">
+                                <a class="text-center btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Read more</a>
                             </div>
                         </div>
                     </div>
                 <?php } ?>
-            </div>
+                </div>
         </div>
         <div class="mt-4">
             <h3 class="pb-3">Scegli tra le cucce disponibili per il tuo Animale</h3>
@@ -65,41 +71,62 @@ $FoodList = [
                 <?php foreach ($DogsBedList as $Dogbed) { ?>
                     <div class="d-flex">
                         <div class="card" style="width: 18rem;">
-                            <img src="<?= $Dogbed->imgProduct ?>" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text m-0">Categoria: <?= $Dogbed->category->icon; $Dogbed->category->name ?></p>
-                                <p class="card-text"><strong>Gioco:</strong> <?= $Dogbed->name ?></p>
-                                <p class="card-text"><strong>Prezzo Gioco:</strong> <?= $Dogbed->getPrice() ?></p>
-                                <p class="card-text"><strong>Descrizione Gioco:</strong> <?= $Dogbed->description ?></p>
-                                <p class="card-text"><strong>Dimensione Cuccia:</strong> <?= $Dogbed->BedDimension ?></p>
-                                <a href="#" class="btn btn-primary">Acquista Prodotto</a>
+                            <div class="container">
+                                <div class="d-flex flex-column justify-content-between">
+                                    <img src="<?= $Dogbed->imgProduct ?>" class="card-img-top" alt="...">
+                                    <div class="card-body text-center">
+                                        <p class="card-text m-0">Categoria: <?= $Dogbed->category->icon; $Dogbed->category->name ?></p>
+                                        <div class="collapse" id="collapseExample1">
+                                            <p class="card-text"><strong>Prezzo Gioco:</strong> <?= $Dogbed->getPrice() ?></p>
+                                            <p class="card-text"><strong>Descrizione Gioco:</strong> <?= $Dogbed->description ?></p>
+                                            <p class="card-text"><strong>Dimensione Cuccia:</strong> <?= $Dogbed->BedDimension ?></p>
+                                            <a href="#" class="btn btn-primary">Acquista Prodotto</a>
+                                        </div>
+                                    </div>
+                                    <div class="text-center mb-3">
+                                        <a class="text-center btn btn-primary" data-bs-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample1">Read more</a>
+                                    </div>
+                                </div>
                             </div>
+                        <?php } ?>
                         </div>
                     </div>
-                <?php } ?>
             </div>
-        </div>
-        <div class="mt-4">
-            <h3 class="pb-3">Scegli tra i cibi per il tuo Animale</h3>
-            <div class="d-flex">
-                <?php foreach ($FoodList as $food) { ?>
-                    <div class="d-flex">
-                        <div class="card" style="width: 18rem;">
-                            <img src="<?= $food->imgProduct ?>" class="card-img-top" alt="...">
-                            <div class="card-body">
-                            <p class="card-text m-0">Categoria: <?= $food->category->icon; $food->category->name ?></p>
-                                <p class="card-text"><strong>Gioco:</strong> <?= $food->name ?></p>
-                                <p class="card-text"><strong>Prezzo Gioco:</strong> <?= $food->getPrice() ?></p>
-                                <p class="card-text"><strong>Descrizione Gioco:</strong> <?= $food->description ?></p>
-                                <p class="card-text"><strong>Dimensione Cuccia:</strong> <?= $food->ingredients ?></p>
-                                <a href="#" class="btn btn-primary">Acquista Prodotto</a>
+            <div class="mt-4">
+                <h3 class="pb-3">Scegli tra i cibi per il tuo Animale</h3>
+                <div class="d-flex">
+                    <?php foreach ($FoodList as $food) { ?>
+                        <div class="d-flex">
+                            <div class="card" style="width: 18rem;">
+                                <div class="container">
+                                    <div class="d-flex flex-column justify-content-between">
+                                        <img src="<?= $food->imgProduct ?>" class="card-img-top" alt="...">
+                                        <div class="card-body text-center">
+                                            <p class="card-text m-0">Categoria: <?= $food->category->icon;$food->category->name ?></p>
+                                            <div class="collapse" id="collapseExample2">
+                                                <p class="card-text"><strong>Prodotto:</strong> <?= $food->name ?></p>
+                                                <p class="card-text"><strong>Prezzo Prodotto:</strong> <?= $food->getPrice() ?></p>
+                                                <p class="card-text"><strong>Descrizione Prodotto:</strong> <?= $food->description ?></p>
+                                                <p class="card-text"><strong>Ingredienti Prodotto:</strong> <?= $food->ingredients ?></p>
+                                                <a href="#" class="btn btn-primary">Acquista Prodotto</a>
+                                            </div>
+                                        </div>
+                                        <div class="text-center mb-3">
+                                            <a class="text-center btn btn-primary" data-bs-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample2">Read more</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php } ?>
                             </div>
                         </div>
-                    </div>
-                <?php } ?>
+                </div>
             </div>
-        </div>
     </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+
+
 </body>
 
 </html>
